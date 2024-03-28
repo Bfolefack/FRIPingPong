09cf68cadc4f6ad830214d5f71abf76177aa53a0
AadhiMS <aadhishanmugham@gmail.com>
2024-03-28 16:16:32 -0500

    Added shape detection, still bugged

--- shapeColorDetected.py
+++ shapeColorDetected.py
@@ -1,3 +1,4 @@
+
 import cv2 as cv 
 import numpy as np
 
@@ -11,7 +12,19 @@ while True:
     ret, ballFrame = ballVideoStream.read()
     if not ret: break
 
-    cv.imshow("ball frame", ballFrame)
+
+    greyBallFrame = cv.cvtColor(ballFrame, cv.COLOR_BGR2GRAY)
+    blurBallFrame = cv.GaussianBlur(greyBallFrame, (9,9), 0) #Reduce Noise
+
+    detectedShape = cv.HoughCircles(blurBallFrame, cv.HOUGH_GRADIENT, dp=1, minDist=50, param1=200, param2=30, minRadius=0, maxRadius=0)
+
+    if detectedShape is not None:
+        detectedShape = np.round(detectedShape[0, :]).astype("int")
+        for (x, y, r) in detectedShape:
+            cv.circle(ballVideoStream, (x, y), r, (0, 255, 0), 2)
+
+
+    cv.imshow("ball frame", ballVideoStream)
     if cv.waitKey(1) & 0xFF == ord('x'): break #Press x to close out webcam window 
 
 ballVideoStream.release()
