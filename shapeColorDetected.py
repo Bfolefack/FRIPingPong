229287a9d6bfefda68cef120322130b9e3e1d779
AbhinavTiruvee <ranjaniabhinav@gmail.com>
2024-04-04 16:34:24 -0500

    code

--- shapeColorDetected.py
+++ shapeColorDetected.py
@@ -1,18 +1,25 @@
 import cv2 as cv 
 import numpy as np
+import pyrealsense2 as rs
+
 
 # Open the video stream
-ballVideoStream = cv.VideoCapture(0)
+pipeline = rs.pipeline()
+config = rs.config()
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+pipeline.start(config)
 
 
 while True:
+    frames = pipeline.wait_for_frames()
     # Read a frame from the video stream
-    ret, ballFrame = ballVideoStream.read()
-    if not ret: 
+    color_frame = frames.get_color_frame()
+    if not color_frame: 
         break
 
     # Convert the frame to grayscale
-    greyBallFrame = cv.cvtColor(ballFrame, cv.COLOR_BGR2GRAY)
+    greyBallFrame = cv.cvtColor(color_frame, cv.COLOR_BGR2GRAY)
 
     # Apply Gaussian blur to reduce noise
     blurBallFrame = cv.GaussianBlur(greyBallFrame, (11, 11), 0)
@@ -24,16 +31,16 @@ while True:
     if detectedCircles is not None:
         detectedCircles = np.round(detectedCircles[0, :]).astype("int")
         for (x, y, r) in detectedCircles:
-            cv.circle(ballFrame, (x, y), r, (0, 255, 0), 2)
+            cv.circle(color_frame, (x, y), r, (0, 255, 0), 2)
             print(x, y)
 
     # Display the frame with detected circles
-    cv.imshow("Ball Frame", ballFrame)
+    cv.imshow("Ball Frame", color_frame)
 
     # Check for the 'x' key press to exit the loop
     if cv.waitKey(1) & 0xFF == ord('x'): 
         break
 
 # Release the video stream and close all OpenCV windows
-ballVideoStream.release()
+color_frame.release()
 cv.destroyAllWindows()
